0d292d80cf3f25e71e27e703e2142e31f9d9e43e
Shujan Mahmud <49782934+SUZON-123@users.noreply.github.com>
2022-02-17 15:01:54 +0600

    bulb on off project
    bulb on off project

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function lightOn(){
+   let x =  document.getElementById("img").src = "img/bulbOn.jpg"
+    
+}
+
+function lightOff(){
+    let x =  document.getElementById("img").src = "img/bulbOff.jpg"
+     
+ }
